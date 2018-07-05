6bd08feb939121909bcbc21cd7771a4ea230a49b
Cujner <Cujner>
2018-07-06 01:30:34 +0200

    Minor Adjustments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log(`I'm a silly entry point aaa`);
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				<h1>Cujo</h1>
+				<p>Test</p>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
