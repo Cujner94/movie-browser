9e4990c56e31f1b311f4c92e6783fa351f324869
Cujner <Cujner>
2018-08-23 15:42:43 +0200

    Starting Point

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'b41936b8ed0f4f2f3e076cf8f2d3af29';
+
 class App extends Component{
 	render(){
 		return(
 			<div>
 				<h1>Cujo</h1>
 				<p>Test</p>
+				<p>Hello</p>
 			</div>
 		)
 	}
