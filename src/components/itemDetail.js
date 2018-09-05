b45ffa7ec6eecbeae5e90995ce861483b8d2dd77
Cujner <Cujner>
2018-09-05 13:50:02 +0200

    1

--- src/components/itemDetail.js
+++ src/components/itemDetail.js
@@ -3,9 +3,81 @@ import React, {Component} from 'react';
 class ItemDetails extends Component{
 	render(){
 		const id = this.props.location.search.slice(4);
-		console.log(id, this.props.match.params.searchType);
+		const searchType = this.props.match.params.searchType;
+		let renderThis;
+		
+		// Checking for what type of component to render
+		switch (searchType) {
+			case 'movie':
+				renderThis = <MovieComponent id={id} />
+				break;
+			case 'tv':
+				renderThis = <TvComponent id={id} />
+				break;
+			case 'person':
+				renderThis = <PersonComponent id={id} />
+				break;	
+		}
+		
+		
 		return(
-			<div>Sup</div>
+			<div>
+				{renderThis}
+			</div>
+		)
+	}
+}
+
+// MOVIE COMPONENT 
+class MovieComponent extends Component{
+	
+	componentDidMount(){
+		console.log("Movie Mounted");
+		const id = this.props.id;
+		// Fetching data for movie detail
+	}
+	
+	render(){
+		return(
+			<div>
+				<h1>Movie Component</h1>
+			</div>
+		)
+	}
+}
+
+// TV COMPONENT
+class TvComponent extends Component{
+	
+	componentDidMount(){
+		console.log("Tv Mounted");
+		const id = this.props.id;
+		// Fetching data for movie detail
+	}
+	
+	render(){
+		return(
+			<div>
+				<h1>Tv Component</h1>
+			</div>
+		)
+	}
+}
+
+// PERSON COMPONENT
+class PersonComponent extends Component{
+	
+	componentDidMount(){
+		console.log("Person Mounted");
+		const id = this.props.id;
+		// Fetching data for person detail
+	}
+	
+	render(){
+		return(
+			<div>
+				<h1>Person Component</h1>
+			</div>
 		)
 	}
 }
